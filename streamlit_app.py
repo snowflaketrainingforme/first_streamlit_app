69427b736685a54f6d62a87640638972dfc18182
snowflake training <121855612+snowflaketrainingforme@users.noreply.github.com>
2023-01-03 14:31:46 +0530

    Update streamlit_app.py
    Added import pandas
    Added 2 lines of code to read data from a csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -8,5 +9,7 @@ streamlit.text('Paneer paratha            RS 80')
 streamlit.text('Gobi paratha              RS 80')
 
 streamlit.header('🍊🍋🍌🥛🧃Make your special smoothie')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 
