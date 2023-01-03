33cfa5a1155d57e67ccf8c09adbe450ad35b5f07
snowflake training <121855612+snowflaketrainingforme@users.noreply.github.com>
2023-01-03 14:51:00 +0530

    Update streamlit_app.py
    Added a couple of fruits in the pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 #setting the index as the fruit name instead of the number
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Apple'])
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
