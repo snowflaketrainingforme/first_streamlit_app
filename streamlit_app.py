8615765e755fde2abe872bd4bce31ad0b9997ed6
snowflake training <121855612+snowflaketrainingforme@users.noreply.github.com>
2023-01-03 14:23:06 +0530

    Update streamlit_app.py
    Added some texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Aloo paratha              RS 80')
+streamlit.text('Aloo pyaaj paratha        RS 80')
+streamlit.text('Paneer paratha            RS 80')
+streamlit.text('Gobi paratha              RS 80')
+
 
